8044da725ccb8e89efeed514d437f22c3bf29361
zdanovich89 <zdanovich89@gmail.com>
2019-12-29 14:33:35 +0300

    feat: create iconBar

---
+++ src/iconBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./iconBar.css";
+
+export function IconBar({orientation}) {
+  return (
+    <div className={`icon-bar_${orientation}`}>
+      <a className='active' href="#"><i className="fa fa-home"></i></a>
+      <a href="#"><i className="fa fa-search"></i></a>
+      <a href="#"><i className="fa fa-envelope"></i></a>
+      <a href="#"><i className="fa fa-globe"></i></a>
+      <a href="#"><i className="fa fa-trash"></i></a>
+    </div>
+  )
+}
